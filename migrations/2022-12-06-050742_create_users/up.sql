05ac91ff297ec0d1d0c366b474f55bdef820533e
Theodore L'Heureux <theodorelheureux@gmail.com>
2022-12-08 03:31:27 -0500

    feat: make user's username and email unique in DB

--- migrations/2022-12-06-050742_create_users/up.sql
+++ migrations/2022-12-06-050742_create_users/up.sql
@@ -1,8 +1,8 @@
 -- Your SQL goes here
 CREATE TABLE users (
     id uuid DEFAULT uuid_generate_v4(),
-    username TEXT NOT NULL,
-    email TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
