886601a3123965944026b8b9f1114519786094ff
Theodore L'Heureux <theodorelheureux@gmail.com>
2022-12-08 21:21:44 -0500

    feat: create vote migration

---
+++ migrations/2022-12-09-021313_create_votes/up.sql
@@ -0,0 +1,10 @@
+-- Your SQL goes here
+CREATE TABLE votes (
+    id uuid DEFAULT uuid_generate_v4(),
+    value SMALLINT NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    user_id uuid NOT NULL,
+    question_id uuid NOT NULL,
+    PRIMARY KEY (id)
+)
