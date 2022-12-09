71014d2524201520c7a04030e0b2ef51540fac29
Theodore L'Heureux <theodorelheureux@gmail.com>
2022-12-08 21:13:01 -0500

    feat: create question migration

---
+++ migrations/2022-12-09-020021_create_questions/up.sql
@@ -0,0 +1,9 @@
+-- Your SQL goes here
+CREATE TABLE questions (
+    id uuid DEFAULT uuid_generate_v4(),
+    text VARCHAR(128) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    user_id uuid NOT NULL,
+    PRIMARY KEY (id)
+)
--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,16 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    questions (id) {
+        id -> Uuid,
+        text -> Varchar,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        user_id -> Uuid,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Uuid,
         username -> Varchar,
@@ -10,3 +20,8 @@ diesel::table! {
         updated_at -> Timestamp,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    questions,
+    users,
+);
