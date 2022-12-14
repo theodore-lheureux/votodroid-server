e24b9d54ebd47570a7cdca6f2b12488240b8456f
Theodore L'Heureux <theodorelheureux@gmail.com>
2022-12-14 14:24:46 -0500

    fix: vote value is unique

--- migrations/2022-12-09-021313_create_votes/up.sql
+++ migrations/2022-12-09-021313_create_votes/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE votes (
     id uuid DEFAULT uuid_generate_v4(),
-    value INTEGER NOT NULL UNIQUE,
+    value INTEGER NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     user_id uuid NOT NULL references users(id),
